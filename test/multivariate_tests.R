0e24563b2afe6bff2a79797d6eb30e6870c6073b
dfleis <david.p.fleischer@gmail.com>
2018-04-01 08:30:53 -0400

    testing ahb cv

--- test/multivariate_tests.R
+++ test/multivariate_tests.R
@@ -4,27 +4,23 @@ library(HierBasis)
 
 #install_github("dfleis/hierbasis2")
 library(hierbasis2)
-source("R/wavelet.R")
 
 #=========================#
 #===== generate data =====#
 #=========================#
-set.seed(124)
-n <- 500
-p <- 5
-sigma <- 0.5
+set.seed(1)
+n <- 300
+p <- 6
+sigma <- 0.3
 
 X <- matrix(rnorm(n * p), nrow = n)
 eps <- rnorm(n, 0, sigma)
 
 # define some arbitrary additive function of the covariates
 # y = sum^p_{j = 1} y_j = sum^p_{j = 1} f_j(X[,j])
-y1 <- X[,1] - 1.5 * X[,1]^3
-y2 <- sin(pi * X[,2]) - 0.5 * sin(0.25 * pi * X[,2])
-y3 <- 0.25 * X[,3]^2
-for (i in 1:3) {
-  y3 <- y3 + 1/i^2 * wavelet(X[,3] - 1/i^2, i)
-}
+y1 <- 2 * sin(X[, 1])
+y2 <- X[, 2]
+y3 <- (X[, 3])^3/10
 
 ytrue <- y1 + y2 + y3
 y <- ytrue + eps
@@ -43,11 +39,11 @@ plot(y ~ X[,1], pch = 19, cex = 0.75,
 #===== fit models =====#
 #======================#
 pt <- proc.time()
-mod1 <- HierBasis::AdditiveHierBasis(x = X, y = y, nbasis = 5)
+mod1 <- HierBasis::AdditiveHierBasis(x = X, y = y, nbasis = 15)
 proc.time() - pt
 
 pt <- proc.time()
-mod2 <- hierbasis2::additivehierbasis(X = X, y = y, nbasis = 5)
+mod2 <- hierbasis2::additivehierbasis(X = X, y = y, nbasis = 15)
 proc.time() - pt
 
 yhat1 <- predict(mod1)
@@ -55,15 +51,13 @@ yhat2 <- predict(mod2)
 sum(abs(yhat1 - yhat2))
 
 pt <- proc.time()
-cv.mod2 <- hierbasis2::cv.additivehierbasis(X = X, y = y, nbasis = 10, nfolds = 10)
+cv.mod2 <- hierbasis2::cv.additivehierbasis(X = X, y = y, nbasis = 15, nfolds = 10)
 proc.time() - pt
-
-m <- cv.init()
 
 #==================#
 #==== figures =====#
 #==================#
-ind.lam <- 40
+ind.lam <- cv.mod2$lambda.min.idx
 
 plot(mod1, ind.func = 1, ind.lam = ind.lam, pch = 19,
      type = 'o', col = 'red', cex = 0.5, lwd = 2)
