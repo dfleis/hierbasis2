cd85f464b08d72a0de504a4a0854daa8ca59d19a
David Fleischer <david.p.fleischer@gmail.com>
2018-05-04 05:49:05 -0400

    temp patch on expand.poly for check.nbasis bug

--- R/expand.poly.R
+++ R/expand.poly.R
@@ -1,5 +1,5 @@
 expand.poly <- function(x, nbasis) {
-  nbasis <- check.nbasis.poly(x, nbasis)
+  nbasis#nbasis <- check.nbasis.poly(x, nbasis)
 
   # generate basis exansion PSI of order 1 to nbasis
   PSI <- outer(x, 1:nbasis, "^")
